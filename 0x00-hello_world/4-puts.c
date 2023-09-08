e30d1afd00f104692753425bab82835461657e1b
Buffalo42 <atustotle@gmail.com>
2023-09-08 04:24:59 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/*
+ *  This C program uses the 'puts' function to print the message:
+ *  "Programming is like building a multilingual puzzle"
+ *  followed by a new line. It then ends the program with the value 0
+ *  to indicate successful execution.
+ */
+
 
 int main(void)
 {
