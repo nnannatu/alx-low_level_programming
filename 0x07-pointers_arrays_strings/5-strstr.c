325f509ea6526568c62c7f84aca104057ac686dc
Buffalo42 <atustotle@gmail.com>
2023-09-26 03:20:44 +0100

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,11 +12,11 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	/**
-	 * 	 * we initialize a helping variable
-	 * 	 	 * to assist in returning one of
-	 * 	 	 	 * our parameters pointers haystack
-	 * 	 	 	 	*/
+/**
+ * we initialize a helping variable
+ * to assist in returning one of
+ * our parameters pointers haystack
+ */
 	char *h, *n;
 
 	while (*haystack != '\0')
