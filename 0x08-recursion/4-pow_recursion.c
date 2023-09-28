2711c4bfdd1077c6aed832f36748c517bfc2572c
Buffalo42 <atustotle@gmail.com>
2023-09-28 23:10:41 +0100

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,17 +10,14 @@
 
 int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+	{
+		return (-1);
+	}
 	if (y == 0)
 	{
 		return (1);
 	}
-
-	if (y % 2 == 0)
-	{
-		return (_pow_recursion(x * x, y / 2));
-	} else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+	return (x * _pow_recursion(x, y - 1));
 }
 
