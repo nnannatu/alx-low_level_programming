79a5fa053289cbd6444fa8a967ae65edafad1476
Buffalo42 <atustotle@gmail.com>
2023-10-12 06:32:39 +0100

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,6 +8,7 @@
  *  @size: size of the array
  *  @action: pointer to function used
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
@@ -17,4 +18,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
+	return;
 }
