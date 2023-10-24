0bdbf90a17048013b3a11c1259d339dbf6570c1f
Buffalo42 <atustotle@gmail.com>
2023-10-24 21:01:57 +0100

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function to free head node..
+ * @head: the pointer to the first node.
+ * Return: *head->n.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr = *head;
+
+	if (*head == NULL)
+		return (0);
+	*head = ptr->next;
+	free(ptr);
+	ptr = *head;
+	return ((*head)->n);
+}
