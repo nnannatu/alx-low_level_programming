736800341649b202b0855f37da3ec536424e0e39
Buffalo42 <atustotle@gmail.com>
2023-10-27 05:23:02 +0100

    task 1 - 5

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int _putchar(char c);
+int get_endianness(void);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int clear_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+
+#endif
